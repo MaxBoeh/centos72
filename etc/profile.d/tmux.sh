14fa93d79f7e5d975fcf628c1d3f728add6015e9
root <root@skynet.salesviewer.com>
2017-03-15 15:21:27 +0100

    do it

--- etc/profile.d/tmux.sh
+++ etc/profile.d/tmux.sh
@@ -1,1 +1,1 @@
-alias tmux="-u -2 -f `[ -f ~/.tmux.conf ] && echo "~/.tmux.conf" || echo "/etc/tmux.conf"`"
+alias tmux="/usr/bin/env tmux -u -2 -f `[ -f ~/.tmux.conf ] && echo "~/.tmux.conf" || echo "/etc/tmux.conf"`"
